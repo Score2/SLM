f1c3ed4abe5cbc576d2c786c07e9e824ba5b21ea
83669 <836696023@qq.com>
2020-09-16 12:53:30 +0800

    更新gradle配置

--- slm-bootstrap/src/main/kotlin/me/scoretwo/simplelibrarymanager/bootstrap/SLMBootstrap.kt
+++ slm-bootstrap/src/main/kotlin/me/scoretwo/simplelibrarymanager/bootstrap/SLMBootstrap.kt
@@ -4,14 +4,10 @@ import me.scoretwo.simplelibrarymanager.commons.SLM
 import me.scoretwo.simplelibrarymanager.commons.bootstrap.SLMMain
 import me.scoretwo.simplelibrarymanager.commons.libraries.LibraryManager
 
-class SLMBootstrap() {
+class SLMBootstrap(slmPlugin: SLMMain) {
 
-    constructor(slmPlugin: SLMMain) {
+    init {
         SLM.bootstrap = slmPlugin
-        init()
-    }
-
-    fun init() {
         LibraryManager.loadLibraries()
         LibraryManager.downloadLibraries()
         LibraryManager.includeLibraries()
